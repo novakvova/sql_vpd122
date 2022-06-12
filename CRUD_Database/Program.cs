8dc65a82f8633a73fc400d968bbcc859215d3f3c
novakvova <novakvova@gmail.com>
2022-06-12 17:41:15 +0300

    Add create and insert

---
+++ CRUD_Database/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Text;
+
+namespace CRUD_Database
+{
+    public class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            UserManager userManager = new UserManager();
+            int action=0;
+
+            do
+            {
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.List Users");
+                Console.WriteLine("2.Insert User");
+                Console.Write("->_");
+                action=int.Parse(Console.ReadLine());
+                switch (action)
+                {
+                    case 1:
+                        {
+                            var users = userManager.Users;
+                            foreach (var item in users)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            var insetUser = new UserCreate();
+                            Console.Write("Enter email: "); insetUser.Email = Console.ReadLine();
+                            Console.Write("Enter firstName: "); insetUser.FirstName = Console.ReadLine();
+                            Console.Write("Enter lastName: "); insetUser.LastName = Console.ReadLine();
+                            var user = userManager.Create(insetUser);
+                            //Console.WriteLine(user);
+                            break;
+                        }
+                }
+
+            } while (action!=0);
+        }
+    }
+}
+
---
+++ CRUD_Database/UserManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Database
+{
+    public class UserManager : IDisposable
+    {
+        private string strCon = "Data Source=DESKTOP-FE25KKP;Initial Catalog=test;Integrated Security=True";
+        private SqlConnection con;
+        public UserManager()
+        {
+            con = new SqlConnection(strCon);
+            con.Open();
+        }
+        public List<User> Users
+        {
+            get
+            {
+                List<User> users = new List<User>();
+                string sql = "Select * from tblUsers";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    var user = new User
+                    {
+                        Id = int.Parse(dr["Id"].ToString()),
+                        Email = dr["Email"].ToString(),
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString()
+                    };
+                    users.Add(user);
+                }
+                return users;
+            }
+        }
+
+
+        public User Create(UserCreate user)
+        {
+            string sql = $"INSERT INTO tblUsers ([Email],[FirstName],[LastName]) VALUES (N'{user.Email}',N'{user.FirstName}',N'{user.LastName}');" +
+                $"SELECT CAST(scope_identity() AS int) ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int id = (int)cmd.ExecuteScalar();
+            
+            return new User
+            {
+                Id=id,
+                Email=user.Email,
+                FirstName=user.FirstName,
+                LastName=user.LastName
+            };
+        }
+        
+        public void Dispose()
+        {
+            con.Close();
+        }
+    }
+}
